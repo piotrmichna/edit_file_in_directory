d41bd47b730363350c64dfbfea4793f86109d687
Piotr Michna <poczta@piotrmichna.pl>
2021-02-27 15:10:43 +0100

    definicja testow dla funckcji file_name_split()

---
+++ format_dir_filename_test.py
@@ -0,0 +1,13 @@
+import pytest
+
+from format_dir_filename import file_name_split
+
+
+@pytest.mark.parametrize('filename, expected', (
+        ('Dokument textowy.txt', ['Dokument textowy', 'txt']),
+        ('Dokument text.txt', ['Dokument text', 'txt']),
+        ('Dokument.txt', ['Dokument', 'txt']),
+        ('Dokument', ['Dokument', False])
+))
+def test_file_name_split(filename, expected):
+    assert expected == file_name_split(filename)
