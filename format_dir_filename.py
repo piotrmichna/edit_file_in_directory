97dfc1df3276bf0627869737439a25f49ec88c26
Piotr Michna <poczta@piotrmichna.pl>
2021-02-27 17:01:07 +0100

    instrukcjie modyfikujace sciezke do pliku

--- format_dir_filename.py
+++ format_dir_filename.py
@@ -32,7 +32,24 @@ def file_name_clean(filename):
     return filename
 
 
-def file_get_new_path(path) -> Path:
+def file_get_new_path(path: Path) -> Path:
+    path = str(path)
+    if '/' in path:
+        file = path.split('/')
+        path_char = '/'
+    elif '\\' in path:
+        file = path.split('\\')
+        path_char = '\\'
+
+    filename = file[-1]
+    filename = file_name_split(filename)
+    filename[0] = file_name_clean(filename[0])
+    if filename[1]:
+        file[-1] = '.'.join(filename)
+    else:
+        file[-1] = filename[0]
+
+    path = path_char.join(file)
     return Path(path)
 
 
